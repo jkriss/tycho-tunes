a5a2d7d806e33a1a41a265ccde2b069d4a601be5
Jesse Kriss <jesse@jklabs.net>
2016-11-18 19:25:50 -0800

    Switching to card reader

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var playlist = require('./playlist')()
 var Path = require('path')
 var Player = require('./player')
 
-// var input = Input({ vid : 1452, pid : 627 }, handleId)
-var input = Input({ path : 'IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/SPI1@15,4/AppleIntelLpssGspi@1/AppleIntelLpssSpiController@1/AppleIntelLpssSpiDevice@0/AppleHSSPIController/Apple Internal Keyboard / Trackpad/Keyboard / Boot/AppleHSSPIHIDDriver' }, handleId)
+var input = Input({ vid : 2303, pid : 9 }, handleId)
+// var input = Input({ path : 'IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/SPI1@15,4/AppleIntelLpssGspi@1/AppleIntelLpssSpiController@1/AppleIntelLpssSpiDevice@0/AppleHSSPIController/Apple Internal Keyboard / Trackpad/Keyboard / Boot/AppleHSSPIHIDDriver' }, handleId)
 
 var libraryRoot = './library/'
 
@@ -12,16 +12,18 @@ var player = Player()
 var isPlaying = false
 
 var specialIds = {
-  '1' : function() {
+  '21848835' : function() {
     console.log("pausing...")
     player.pause()
     // player.stop()
   },
-  '2' : function() {
+  // '2' : function() {
+  '21855731' : function() {
     console.log("previous...")
     player.previous()
   },
-  '3' : function() {
+  // '3' : function() {
+  '22953027' : function() {
     console.log("next...")
     player.next()
   },
--- input.js
+++ input.js
@@ -1,39 +1,46 @@
 var hid = require('hidstream')
+var HID = require('node-hid')
 
 module.exports = function(opts, listener) {
 
-  var device = new hid.device({ parser : hid.parser.keyboard, path : opts.path })
+  // var devices = HID.devices()
+  // console.log("devices:", devices)
 
-  var buffer = ""
-  var debounce;
+  // var device = new hid.device({ parser : hid.parser.keyboard, path : opts.path })
+  var device = new hid.device({ parser : hid.parser.newline, path : opts.path, vid : opts.vid, pid : opts.pid })
+
+  // var buffer = ""
+  // var debounce;
 
   device.on("data", function(data) {
-    if (data.keyCodes[0] === 41) device.close() // close on esc
-
-    // ignore multiples?
-    var primaryChar
-    var charCount = 0
-    data.charCodes.forEach(function(char) {
-      if (char !== '') {
-        primaryChar = char
-        charCount++
-      }
-    })
-    if (charCount === 1) getChar(primaryChar)
+    console.log(data)
+    listener(data.trim())
+    // if (data.keyCodes[0] === 41) device.close() // close on esc
+    //
+    // // ignore multiples?
+    // var primaryChar
+    // var charCount = 0
+    // data.charCodes.forEach(function(char) {
+    //   if (char !== '') {
+    //     primaryChar = char
+    //     charCount++
+    //   }
+    // })
+    // if (charCount === 1) getChar(primaryChar)
   })
 
-  function getChar(char) {
-    // console.log("typed:", char)
-    buffer += char
-    if (debounce) clearTimeout(debounce)
-    debounce = setTimeout(send, 200)
-  }
-
-  // if it's been quiet for 500 ms, send the buffer
-  function send() {
-    listener(buffer)
-    buffer = ""
-  }
+  // function getChar(char) {
+  //   // console.log("typed:", char)
+  //   buffer += char
+  //   if (debounce) clearTimeout(debounce)
+  //   debounce = setTimeout(send, 200)
+  // }
+  //
+  // // if it's been quiet for 500 ms, send the buffer
+  // function send() {
+  //   listener(buffer)
+  //   buffer = ""
+  // }
 
   return {
     close : function() {
